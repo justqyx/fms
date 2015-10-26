ca310709a9a8482a06086eaf41b34e86a7970f8f
nimojs <nimo.jser@gmail.com>
2015-10-26 17:22:05 +0800

    http://fms.help/
    前端数据模拟环境，提供 AJAX 模拟与后端 MVC 中 View 层渲染模拟解决方案。

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+                                                                        /*
+    Docs - 文档         : http://fms.help
+    Demo - 示例      : http://demo.fms.help
+    Community - 社区    : https://github.com/nimojs/fms/issues
+    Github             : https://github.com/nimojs/fms
+    Contact - 联系作者  : nimo.jser[at]gmail.com http://weibo.com/nimojs
+    Help - 帮助 : https://github.com/nimojs/fms/issues/new
+                                                                        */
+var ajax = require('./lib/ajax')
+var doc = require('./lib/doc')
+module.exports = {
+    run: require('./lib/init'),
+    app: null,
+    _set: function (name, value) {
+        this[name] = value
+    },
+    ajax: ajax.ajax,
+    get: ajax.get,
+    post: ajax.post,
+    delete: ajax.delete,
+    put: ajax.put,
+    view: require('./lib/view'),
+    doc: doc.addDoc,
+    docFile: doc.docFile
+}
